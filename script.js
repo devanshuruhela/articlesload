65efd54223412c3e9e3760361f92654299933671
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-09-20 23:51:00 +0530

    Creating post dynamically

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const apikey = "DEMO_KEY";
 const apiurl = `https://api.nasa.gov/planetary/apod?api_key=${apikey}&count=${10}`;
 
+//Elements
+const resultsNav = document.getElementById('resultsNav');
+const favouritesNav = document.getElementById('favouritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const savedConfirmed = document.querySelector('saved-confirmed');
+const loader = document.querySelector('.loader');
+
 let resultsarray = [];
+// to create dom dynamically
+function createdom()
+{
+  resultsarray.forEach(result=>
+    {
+      //card
+      const card = document.createElement('div');
+      card.classList.add('card');
+      const link = document.createElement('a');
+      //link
+      link.href = result.hdurl;
+      link.title = 'View Full Image';
+      link.target = "_blank";
+      //image
+      const image = document.createElement('img')
+      image.classList.add('card-img-top')
+      image.src = result.url;
+      image.alt = "Nasa image of the Day";
+      image.loading = 'lazy';
+
+      //full card
+      const cardbody = document.createElement('div');
+      cardbody.classLis.add('icard-body');
+      
+    })
+}
+
 
 async function getnasaimages() {
   try {
